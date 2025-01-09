3f5f92d6fbb1a3163af76a780ccc0ceb9a9020fa
UserNotFound <102423854+usernotfound999@users.noreply.github.com>
2025-01-09 20:07:04 +0800

    Create c#.cs

---
+++ C#/c#.cs
@@ -0,0 +1,12 @@
+using System;
+namespace WW
+{
+    class W
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("W");
+            Console.ReadKey();
+        }
+    }
+}
